0b36a65fc0b5660318ed73afb9d4a1c83fb2237f
AkashBarve <akashbarve0511@gmail.com>
2019-03-12 22:18:30 -0400

    client

--- src/com/Client.java
+++ src/com/Client.java
@@ -1,4 +1,42 @@
 package com;
 
-public class Client {
+import java.rmi.Remote;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Client class
+ */
+public class Client implements Runnable {
+    private ExecutorService outThreads;
+    private Thread runningThread;
+    private HashMap<Integer, RemotePeer> peersToConn;
+
+    public Client(HashMap<Integer, RemotePeer> peersToConn) {
+        this.peersToConn = peersToConn;
+        this.outThreads = Executors.newFixedThreadPool(this.peersToConn.size());
+    }
+
+    @Override
+    public void run() {
+
+        synchronized (this) {
+            this.runningThread = Thread.currentThread();
+        }
+        for (Integer key : peersToConn.keySet()) {
+            RemotePeer remotePeer = peersToConn.get(key);
+            try {
+                this.outThreads.execute(
+                        //TODO: Implement outgoing request handler
+                        //new OutgoingResquestHanndler(remotePeer)
+                );
+            }
+            catch (Exception ex) {
+                throw new RuntimeException();
+            }
+        }
+
+        this.outThreads.shutdown();
+    }
 }
